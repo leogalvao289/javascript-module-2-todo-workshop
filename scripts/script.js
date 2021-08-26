53324ca354d27a295e215933804c0ea514f916b5
leogalvao289 <leogalvao289@gmail.com>
2021-08-26 17:55:29 +0200

    update

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,6 @@
  
 
-/* Exercise 1
+/* Exercise 1 */
 
 alert('Bienvenido!')
 document.querySelector('#new-todo').addEventListener('submit', (e) => {
@@ -14,7 +14,7 @@ document.querySelector('#new-todo').addEventListener('submit', (e) => {
 //document.querySelectorAll('p')          // => seleciona todos los elementos que cumplan una condicion
 
 
-/* Exercise 2 */ /*Exercise 3 
+/* Exercise 2 */ /*Exercise 3 */
 
 let prueba = document.querySelector('.button')  //=> => permite buscar uns tag por el nombre del selector 
 prueba.style.backgroundColor = 'green'
@@ -25,7 +25,7 @@ function alertSomething () {
   alert('Something')
 }
 
-/*Ejercicio 4 
+/*Ejercicio 4 */
 
 document.querySelector('#new-todo').addEventListener('submit', (alertSomething));
  
@@ -89,7 +89,7 @@ const generateTodoDOM  = todo => {
 }
 
 
-/* Ejercicio 7 
+/* Ejercicio 7 */
 
 const renderTodos = (todos) => {
   const todoList = document.querySelector('#todos')
@@ -165,3 +165,141 @@ const generateTodoDOM = (todo) => {
 
 
 /* Ejercicio 11 */ 
+
+const createTodo = (text) => {
+  todos.push({
+      title: text,
+      completed: false
+  })
+}
+
+/* Ejercicio 12 */ 
+
+const generateTodoDOM = (todoObj) => {
+  const todoEl = document.createElement('label')
+  const containerEl = document.createElement('div')
+  const todoText = document.createElement('span')
+
+  // Setup todo checkbox
+  const checkbox = document.createElement('input')
+  checkbox.setAttribute('type', 'checkbox')
+  checkbox.checked = todoObj.completed
+  containerEl.appendChild(checkbox)
+  checkbox.addEventListener('change', () => {
+      toggleTodo(todoObj.title)
+      renderTodos(todos)
+  })
+
+  // Setup the todo text
+  todoText.textContent = todoObj.title
+  containerEl.appendChild(todoText)
+
+  // Setup container
+  todoEl.classList.add('list-item')
+  containerEl.classList.add('list-item__container')
+  todoEl.appendChild(containerEl)
+
+  // Setup the remove button
+  const removeButton = document.createElement('button')
+  removeButton.textContent = 'remove'
+  removeButton.classList.add('button', 'button--text')
+  todoEl.appendChild(removeButton)
+  removeButton.addEventListener('click', () => {
+      removeTodo(todoObj.title)
+      renderTodos(todos)
+  })
+
+  return todoEl
+}
+
+/* Ejercicio 13  */ 
+
+const toggleTodo = (title) => {
+  const todo = todos.find((todo) => todo.title.toLowerCase() === title.toLowerCase())
+
+  if (todo) {
+      todo.completed = !todo.completed
+  }
+}
+
+const removeTodo = (title) => {
+  const todoIndex = todos.findIndex((todo) => todo.title.toLowerCase() === title.toLowerCase())
+
+  if (todoIndex > -1) {
+      todos.splice(todoIndex, 1)
+  }
+}
+
+/* Ejercicio 14 */ 
+
+const filters = {
+  searchTitle: '',
+  showFinished: false,
+  showUnfinished: false
+}
+
+
+/* Ejercicio 15 */ 
+
+const setFilters = (updates) => {
+  if (typeof updates.searchTitle === 'string') {
+      filters.searchTitle = updates.searchTitle
+  }
+  if (typeof updates.showFinished === 'boolean') {
+      filters.showFinished = updates.showFinished
+  }
+  if (typeof updates.showUnfinished === 'boolean') {
+      filters.showUnfinished = updates.showUnfinished
+  }
+}
+
+/* Ejercicio 16 */ 
+
+document.querySelector('#search-text').addEventListener('input', (e) => {
+  setFilters({
+      searchTitle: e.target.value
+  })
+  renderTodos(todos)
+})
+
+document.querySelector('#show-finished').addEventListener('change', (e) => {
+  setFilters({
+      showFinished: e.target.checked
+  })
+  renderTodos(todos)
+})
+
+document.querySelector('#show-unfinished').addEventListener('change', (e) => {
+  setFilters({
+      showUnfinished: e.target.checked
+  })
+  renderTodos(todos)
+})
+
+/* Ejercicio 17 */
+
+const renderTodos = (todos) => {
+  // filtered Todos
+  let filteredTodos = todos.filter((todo) => todo.title.toLowerCase().includes(filters.searchTitle.toLowerCase()))
+  if(filters.showFinished && filters.showUnfinished) {
+    // do nothing
+  } else if(filters.showFinished) {
+    filteredTodos = filteredTodos.filter((todo) => todo.completed)
+  } else if(filters.showUnfinished) {
+    filteredTodos = filteredTodos.filter((todo) => !todo.completed)
+  }
+  
+  const todoList = document.querySelector('#todos')
+  todoList.innerHTML = ''
+
+  if (filteredTodos.length > 0) {
+      filteredTodos.forEach((todo) => {
+          todoList.appendChild(generateTodoDOM(todo))
+      })
+  } else {
+      const messageEl = document.createElement('p')
+      messageEl.classList.add('empty-message')
+      messageEl.textContent = 'There are no todos to show'
+      todoList.appendChild(messageEl)
+  }
+}
